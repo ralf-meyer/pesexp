d8c47496a923fa636f125eaf81272bfe5b2e9535
Ralf Meyer <meyer.ralf@yahoo.com>
2022-05-18 17:10:52 -0400

    move calculators to subdir

--- pesexp/calculators/calculators.py
+++ pesexp/calculators/calculators.py
@@ -7,23 +7,6 @@ from ase.calculators.calculator import (Calculator, FileIOCalculator,
 from openbabel import openbabel
 
 
-_xtb_methods = ['xtb']
-_openbabel_methods = ['uff', 'mmff94', 'gaff']
-_available_methods = _xtb_methods + _openbabel_methods
-
-
-def get_calculator(method):
-    if method.lower() in _xtb_methods:
-        from xtb.ase.calculator import XTB
-        methods = dict(xtb=('GFN2-xTB', 300.0),
-                       gfnff=('GFNFF', 0.0))
-        # TODO: can't get GFNFF to work!
-        return XTB(method=methods[method][0],
-                   electronic_temperature=methods[method][1])
-    elif method.lower() in _openbabel_methods:
-        return OpenbabelFF(ff=method.upper())
-
-
 class TeraChem(FileIOCalculator):
     """
     TeraChem calculator
