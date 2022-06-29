f41dd7f04043f6a65f0dc1fd883de6b145ee3c2e
Ralf Meyer <meyer.ralf@yahoo.com>
2022-06-29 17:12:09 -0400

    Add test for parse_opt_args()

---
+++ tests/cmds/test_params.py
@@ -0,0 +1,19 @@
+from pesexp.cmds.params import parse_opt_args
+
+
+def test_opt_defaults():
+    params = parse_opt_args(['terachem.inp'])
+    assert params['coords'] == 'dlc'
+    assert params['maxiter'] == 300
+    assert params['hessian_guess'] == 'trivial'
+    assert params['hessian_thresh'] is None
+
+
+def test_parse_opt_args():
+    params = parse_opt_args(['--coords', 'cart', '--maxiter', '400',
+                             '--hessian_guess', 'lindh',
+                             '--hessian_thresh', '1e-3', 'terachem.inp'])
+    assert params['coords'] == 'cart'
+    assert params['maxiter'] == 400
+    assert params['hessian_guess'] == 'lindh'
+    assert params['hessian_thresh'] == 0.001
