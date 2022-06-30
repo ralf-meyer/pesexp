10e67805a8626fe45e28145b399e2498203c15dc
Ralf Meyer <meyer.ralf@yahoo.com>
2022-06-30 11:15:02 -0400

    Dummy implementation of GPROptimizer

--- pesexp/optimizers/ml_optimizers.py
+++ pesexp/optimizers/ml_optimizers.py
@@ -1,49 +1,25 @@
-import numpy as np
+from pesexp.hessians.hessian_approximations import BFGSHessian
 from pesexp.optimizers.optimizers import InternalCoordinatesOptimizer
-from pesexp.ml_calculators.gpr_calculator import GPRCalculator
-from pesexp.ml_calculators.kernels import RBFKernel
+from pesexp.optimizers import BFGS
 
 
-class RestrictedVarianceOptimizer(InternalCoordinatesOptimizer):
+class GPROptimizer(InternalCoordinatesOptimizer):
+    hessian_approx = BFGSHessian
 
     def initialize(self):
-        if np.size(self.H0) == 1:
-            H0 = np.eye(self.coord_set.size()) * self.H0
-        else:
-            H0 = self.coord_set.hessian_to_internals(
-                self.atoms.get_positions(), self.H0)
-        self.H = self.hessian_approx(H0)
-        self.r0 = None
-        self.f0 = None
-        self.e0 = None
-
-        # GPR initialization:
-        kernel = RBFKernel()
-        self.ml_calculator = GPRCalculator(kernel=kernel, normalize_y='max+10')
-        self._hyperparameter_from_hessian()
-
-    def _hyperparameter_from_hessian(self):
-        self.ml_calculator.kernel.length_scale = 1.0
+        return super().initialize()
+        # Initialize GPR
 
     def internal_step(self, f):
-        return -f
+        ml_atoms = self.atoms.copy()
+        ml_atoms.calc = self.ml_calc
+        opt = BFGS(ml_atoms, coordinate_set=self.coord_set)
+        opt.run(0.01)
+        return self.coord_set.diff_internals(ml_atoms.get_positions(),
+                                             self.atoms.get_positions())
 
     def update(self, r, f, r0, f0):
-        if r0 is None or f0 is None:  # No update on the first iteration
-            return
+        super().update(r, f, r0, f0)
+        # Determine hyperparameters
 
-        dr = self.coord_set.diff_internals(r, r0).flatten()
-
-        if np.abs(dr).max() < 1e-7:
-            # Same configuration again (maybe a restart):
-            return
-
-        dg = -(f - f0)
-        self.H.update(dr, dg)
-
-        # Update GPRCalculator
-        self.ml_calculator.add_data(self.atoms)
-        # Update kernel hyperparameter
-        self._hyperparameter_from_hessian()
-        # Refit GPRCalculator
-        self.ml_calculator.fit()
+        # Fit GPR
