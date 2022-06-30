ea9050187a52c6e24ce83d52ee9c35f790bdd88e
Ralf Meyer <meyer.ralf@yahoo.com>
2022-06-30 10:48:20 -0400

    Additional help strings

--- pesexp/cmds/params.py
+++ pesexp/cmds/params.py
@@ -6,18 +6,20 @@ def parse_opt_args(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, help='Terachem input file')
     preproc = parser.add_argument_group('preprocessing',
-                                        'Options for preprocessing steps.')
-    preproc.add_argument('--preopt', type=str)
+                                        'Options for preprocessing steps')
+    preproc.add_argument('--preopt', type=str, help='Currently unused')
     opt = parser.add_argument_group('optimization',
                                     'Options for the main geometry '
-                                    'optimization procedure.')
+                                    'optimization procedure')
     opt.add_argument('--optimizer', type=str, choices=['BFGS', 'RFO'])
     opt.add_argument('--coords', type=str, choices=['cart', 'dlc', 'anc'],
                      default='dlc')
-    opt.add_argument('--maxiter', type=int, default=300)
+    opt.add_argument('--maxiter', type=int, default=300,
+                     help='Maximum number of iterations, defaults to 300')
     opt.add_argument('--hessian_guess', type=str, default='trivial',
                      choices=['trivial', 'schlegel', 'fischer_almloef',
-                              'lindh'])
+                              'lindh'],
+                     help='Method to generate the initial hessian guess')
     opt.add_argument('--hessian_thresh', type=float, help='')
 
     optimize_args = parser.parse_args(args)
