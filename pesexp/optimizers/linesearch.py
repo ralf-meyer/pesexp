acbdb8ee55f52bcbec3e34f964e9cb6676707b3e
Ralf Meyer <meyer.ralf@yahoo.com>
2022-08-11 11:57:10 -0400

    Add Banerjee step control algorithm

--- pesexp/optimizers/linesearch.py
+++ pesexp/optimizers/linesearch.py
@@ -14,32 +14,112 @@ def backtracking(opt, c1=1e-4):
             self.xyz_prev = self.atoms.get_positions()
             self.e_prev = np.inf
             self.f_prev = np.zeros_like(self.xyz_prev)
-            self.step_prev = np.zeros_like(self.xyz_prev)
 
         def step(self, f=None):
             if f is None:
                 f = self.atoms.get_forces()
             e = self.atoms.get_potential_energy()
 
+            step_prev = self.atoms.get_positions() - self.xyz_prev
             # Check sufficient decrease condition:
             # Scaling parameter alpha and step direction p are combined in
             # self.step_prev. The minus sign is due to the use of force instead
             # of gradient. np.vdot is used to flattten the arrays f_prev and step_prev
-            if e < self.e_prev - self.c1 * np.vdot(self.f_prev, self.step_prev):
+            if e < self.e_prev - self.c1 * np.vdot(self.f_prev, step_prev):
                 # Last step was accepted: save current values
                 self.e_prev = e
                 self.f_prev = f
                 self.xyz_prev = self.atoms.get_positions()
                 # Take unscaled step:
                 super().step()
-                # Save step
-                self.step_prev = self.atoms.get_positions() - self.xyz_prev
             else:  # Retake last step with smaller alpha
                 logger.info(
                     f"Rejecting step {self.nsteps} - delta E = {e - self.e_prev:.6f}"
                 )
-                self.step_prev = 0.5 * self.step_prev
-                self.atoms.set_positions(self.xyz_prev + self.step_prev)
+                self.atoms.set_positions(self.xyz_prev + 0.5 * step_prev)
 
     BacktrackingOptimizer.c1 = c1
     return BacktrackingOptimizer
+
+
+def banerjee_step_control(opt, threshold=0.3, min_reduction=0.5):
+    class BanerjeeStepControlledOptimizer(opt):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.xyz_prev = self.atoms.get_positions()
+            self.e_prev = None
+            self.f_prev = np.zeros_like(self.xyz_prev)
+
+        def step(self, f=None):
+            if f is None:
+                f = self.atoms.get_forces()
+            e = self.atoms.get_potential_energy()
+
+            # In the first step e_prev is None and we do not need to check any other
+            # conditions
+            if self.e_prev is not None:
+                actual_delta_e = e - self.e_prev
+                f_prev_internal = self.coord_set.force_to_internals(
+                    self.xyz_prev, self.f_prev
+                )
+                step_internal = self.coord_set.diff_internals(
+                    self.atoms.get_positions(), self.xyz_prev
+                )
+                # Eq. (7) in Banerjee et al. (but in internal coordinates)
+                model_delta_e = (
+                    np.dot(step_internal, -f_prev_internal)
+                    + 0.5
+                    * np.dot(
+                        step_internal,
+                        np.dot(self.H, step_internal),
+                    )
+                ) / (1.0 + np.dot(step_internal, step_internal))
+                # Unsure why the relative error is calculated by dividing by the model
+                # error and not the actual error. Simply following Banerjee et al. here:
+                delta = np.abs(actual_delta_e - model_delta_e) / np.abs(model_delta_e)
+                logger.debug(
+                    f"Step: {self.nsteps}: delta E, "
+                    f"actual {actual_delta_e:.5f}, "
+                    f"model {model_delta_e:.5f} - "
+                    f"relative error of delta E = {delta:.2f}"
+                )
+                # Check relative error of model predicted energy change
+                if delta > self.threshold:  # Retake last step with smaller alpha
+                    logger.info(
+                        f"Rejecting step {self.nsteps} - "
+                        f"relative error of delta E = {delta:.2f} > threshold"
+                    )
+                    norm_x = np.linalg.norm(step_internal)
+                    u = step_internal / norm_x
+                    g = np.dot(-f_prev_internal, u)
+                    h = np.dot(u, np.dot(self.H, u))
+                    M = 6 * np.abs(actual_delta_e - model_delta_e) / norm_x**3
+                    t_vec = np.linspace(0, norm_x, 101)
+                    inds = np.where(
+                        M * t_vec**2 / 6
+                        <= self.threshold * np.abs(g + 0.5 * h * t_vec)
+                    )
+                    t = t_vec[inds[0][-1]]
+                    # Ensure that there is always some reduction:
+                    t = min(t, self.min_reduction * norm_x)
+                    logger.debug(
+                        f"Rescaling last step by {t/norm_x:.2f}, "
+                        f"new norm_x = {t:.2f} "
+                        f"original norm_x = {norm_x:.2f}"
+                    )
+                    self.atoms.set_positions(
+                        self.coord_set.to_cartesians(t * u, self.xyz_prev)
+                    )
+                    return
+            # Last step was accepted: save current values
+            self.e_prev = e
+            self.f_prev = f
+            self.xyz_prev = self.atoms.get_positions()
+            # Take unscaled step:
+            super().step()
+            # Save step
+            self.step_prev = self.atoms.get_positions() - self.xyz_prev
+
+    BanerjeeStepControlledOptimizer.threshold = threshold
+    BanerjeeStepControlledOptimizer.min_reduction = min_reduction
+    return BanerjeeStepControlledOptimizer
--- tests/optimizers/test_linesearch.py
+++ tests/optimizers/test_linesearch.py
@@ -1,10 +1,14 @@
 import ase.atoms
 import ase.calculators.emt
-from pesexp.optimizers import BFGS
-from pesexp.optimizers.linesearch import backtracking
+import numpy as np
+from pesexp.calculators.calculators import ThreeDCalculator
+from pesexp.optimizers import BFGS, PRFO
+from pesexp.optimizers.linesearch import backtracking, banerjee_step_control
+from pesexp.geometry.primitives import Cartesian
+from pesexp.geometry.coordinate_systems import InternalCoordinates
 
 
-def test_convergence_criteria():
+def test_backtracking():
     """This test uses a bad guess for the initial hessian to provoke large step
     sizes and tests that the optimization converges if backtracking line search
     is used."""
@@ -13,4 +17,27 @@ def test_convergence_criteria():
 
     opt = backtracking(BFGS)(atoms, H0=1.0)
     opt.run(steps=100)
+    assert opt.converged()
+
+
+def test_banerjee_step_control():
+    class DummyCalc(ThreeDCalculator):
+        def energy(self, x, y, z):
+            return x**4 - y**2 + 0.001 * z**2
+
+        def gradient(self, x, y, z):
+            return 4 * x**3, -2 * y, 0.002 * z
+
+    atoms = ase.atoms.Atoms(positions=np.array([[1.0, 1.0, 1.0]]))
+    atoms.calc = DummyCalc()
+    coord_set = InternalCoordinates(
+        [Cartesian(0, axis=0), Cartesian(0, axis=1), Cartesian(0, axis=2)]
+    )
+
+    # Starting from exact Hessian
+    H0 = np.array([[12.0, 0.0, 0.0], [0.0, -2.0, 0.0], [0.0, 0.0, 0.002]])
+    opt = banerjee_step_control(PRFO)(
+        atoms, coordinate_set=coord_set, H0=H0, maxstep=0.05
+    )
+    opt.run(fmax=0.001, steps=100)
     assert opt.converged()
